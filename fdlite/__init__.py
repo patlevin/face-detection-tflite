beab0efd4342dfdd37e6aa4b580b7efe8fdf704a
patlevin <levin.patrick@gmail.com>
2021-05-30 21:17:51 +0200

    add face_landmark_to_render_data, update version

--- fdlite/__init__.py
+++ fdlite/__init__.py
@@ -2,9 +2,10 @@
 from .face_detection import FaceDetection, FaceDetectionModel     # noqa:F401
 from .face_detection import FaceIndex                             # noqa:F401
 from .face_landmark import FaceLandmark, face_detection_to_roi    # noqa:F401
+from .face_landmark import face_landmarks_to_render_data          # noqa:F401
 from .iris_landmark import IrisIndex, IrisLandmark, IrisResults   # noqa:F401
 from .iris_landmark import eye_landmarks_to_render_data           # noqa:F401
 from .iris_landmark import iris_depth_in_mm_from_landmarks        # noqa:F401
 from .iris_landmark import iris_landmarks_to_render_data          # noqa:F401
 from .iris_landmark import iris_roi_from_face_landmarks           # noqa:F401
-__version__ = '0.2.0'
+__version__ = '0.3.0'
